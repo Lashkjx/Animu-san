304a2212257595fed87dcc48cfcf48a933fba88f
Kanan Lashkjx <Lashkjx@gmail.com>
2019-06-01 08:10:33 -0300

    Preparing workbench to rss support

--- puya/__init__.py
+++ puya/__init__.py
@@ -1,15 +1,8 @@
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from pages.PuyaSubs import puya
+from UI.PuyaSubs import puya
 from pages.Spreadsheet import *
-from resources.Notifications import *
 import os
-import sys
 
 
 def clean():
