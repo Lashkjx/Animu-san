7caf73e9af49208255af557172566c20f604086b
Kanan Lashkjx <Lashkjx@gmail.com>
2019-06-01 07:53:29 -0300

    Working, opening the magnet links directly.

--- puya/__init__.py
+++ puya/__init__.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.keys import Keys
@@ -19,17 +20,23 @@ def closeDriver(driver):
     driver.close()
 
 
-def initializeDriver(url):
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument("--headless")
-    chromeDriver = webdriver.Chrome(executable_path='chromedriver.exe',chrome_options=chrome_options)
-    chromeDriver.get(url)
-    return chromeDriver
-
+def initialize(browser,url):
+    if browser == "chrome":
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument("--headless")
+        chromeDriver = webdriver.Chrome(executable_path='chromedriver.exe',chrome_options=chrome_options)
+        chromeDriver.get(url)
+        return chromeDriver
+    elif browser == "firefox":
+        options = Options()
+        options.headless = True
+        firefoxDriver = webdriver.Firefox(options=options, executable_path=r'geckodriver.exe')
+        firefoxDriver.get(url)
+        return firefoxDriver
 
 print("       - Welcome Senpai! to ANIMU-SAN v.1.0.0 - \n")
 
-driver = initializeDriver('http://www.puya.si')
+driver = initialize("firefox", 'http://www.puya.moe')
 animeEntries = animeEntriesGS()
 animeStop = animeStopGS()
 for anime in animeEntries:
--- puya/pages/PuyaSubs.py
+++ puya/pages/PuyaSubs.py
@@ -6,38 +6,39 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from resources.Notifications import *
 import os
+import time
 import sys
 
 def puya(driver, pattern, stop):
-    match = False
-    option = None
     animeArticle = driver.find_elements_by_css_selector("article")
     for anime in animeArticle:
         title = anime.find_element_by_css_selector("h2")
-        download = anime.find_elements_by_css_selector("div[style]")
-        for option in download:
-            if '720p' in option.text:
-                downloadButton = option.find_element_by_css_selector("a:first-child")
+        titlePush = title.text
         if title.text.startswith(pattern):
             match = True
-            downloadUrl = downloadButton.get_attribute('href')
-            driver.get(downloadUrl)
+            download = anime.find_elements_by_css_selector("p")
+            for option in download:
+                if '720p' in option.text:
+                    downloadButton = option.find_element_by_css_selector("a:first-child")
+                    downloadUrl = downloadButton.get_attribute('href')
+                    driver.get(downloadUrl)
+                    break
             refresh = True
             while refresh:
                 try:
                     resolutionTitle = driver.find_element_by_css_selector("h3:nth-child(1)")
-                    torrentButton = driver.find_element_by_css_selector("a.btn.btn-default:nth-child(1)")
+                    torrentButton = driver.find_element_by_css_selector(".panel-footer a:nth-child(2)")
                     refresh = False
                 except Exception:
                     driver.refresh()
                     myElem = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'h3:nth-child(1)')))
                     resolutionTitle = driver.find_element_by_css_selector("h3:nth-child(1)")
-                    torrentButton = driver.find_element_by_css_selector("a.btn.btn-default:nth-child(2)")
+                    torrentButton = driver.find_element_by_css_selector(".panel-footer a:nth-child(2)")
                     refresh = True
             if '[720p]' in resolutionTitle.text:
-                torrentButton.click()
-                # pushNotification(title.text)
-                driver.get('http://www.puya.si')
+                os.startfile(torrentButton.get_attribute('href'))
+                pushNotification(titlePush)
+                driver.get('http://www.puya.moe')
             else:
                 print('Wrong resolution')
             return driver
