ebf32c61e345524849d8cd0e3d9101c10cea78d1
Kanan Lashkjx <Lashkjx@gmail.com>
2019-06-01 18:53:58 -0300

    First release.

--- puya/pages/Spreadsheet.py
+++ puya/pages/Spreadsheet.py
@@ -4,7 +4,7 @@ from oauth2client.service_account import ServiceAccountCredentials as sac
 
 # Variables.
 scope = ['https://www.googleapis.com/auth/drive']
-cred = sac.from_json_keyfile_name('pages/my_credential.json', scope)
+cred = sac.from_json_keyfile_name('C:/Users/Kanan Lashkjx/.oauth2/my_credential.json', scope)
 client = gs.authorize(cred)
 
 # Google sheet file.
