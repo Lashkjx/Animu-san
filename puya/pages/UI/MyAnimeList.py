91f73ce4cbfd26c5f3089dd18ad4180a97467d2b
Kanan Lashkjx <Lashkjx@gmail.com>
2019-06-29 16:57:21 -0300

    Added details to the result is the expected.

--- puya/pages/UI/MyAnimeList.py
+++ puya/pages/UI/MyAnimeList.py
@@ -5,17 +5,18 @@ from resources.CSV.CSV import *
 
 
 def searchTitle(title):
-    for result in search(title + " mal", tld="com", num=1, stop=1, pause=0):
+    for result in search(title + " anime mal", tld="com", num=1, stop=1, pause=0):
         break
     req = requests.get(result)
     soup = BeautifulSoup(req.content, 'html.parser')
     title = soup.select('h1')[0].text.strip()
-    type = soup.find(text='Type:', attrs={'class': 'dark_text'}).find_next_sibling().get_text()
     duration_mal = soup.find(text='Duration:',
                              attrs={'class': 'spaceit_pad', 'class': 'dark_text'}).next_sibling.strip()
+    type = soup.find(text='Type:', attrs={'class': 'dark_text'}).find_next_sibling().get_text()
     duration = duration_mal[:duration_mal.find('.')] + "."
     newEntry = [title, type, duration, 'FALSE', 'Rewatching']
     add_entry_csv("Anime register", newEntry)
     req.cookies.clear()
+
 
 
