3f1fb341d96f9585da87c757cedd9a6ffcd50395
Kanan Lashkjx <Lashkjx@gmail.com>
2019-06-29 18:17:46 -0300

    Added search on CSV.

--- puya/pages/UI/MyAnimeList.py
+++ puya/pages/UI/MyAnimeList.py
@@ -4,7 +4,7 @@ import requests
 from resources.CSV.CSV import *
 
 
-def searchTitle(title):
+def searchTitle(title, is_seasonal, status):
     for result in search(title + " anime mal", tld="com", num=1, stop=1, pause=0):
         break
     req = requests.get(result)
@@ -14,9 +14,9 @@ def searchTitle(title):
                              attrs={'class': 'spaceit_pad', 'class': 'dark_text'}).next_sibling.strip()
     type = soup.find(text='Type:', attrs={'class': 'dark_text'}).find_next_sibling().get_text()
     duration = duration_mal[:duration_mal.find('.')] + "."
-    newEntry = [title, type, duration, 'FALSE', 'Rewatching']
+    newEntry = [title, type, duration, is_seasonal.upper(), status]
     add_entry_csv("Anime register", newEntry)
     req.cookies.clear()
 
-
+show_csv_content("Anime register")
 
--- puya/resources/CSV/CSV.py
+++ puya/resources/CSV/CSV.py
@@ -1,25 +1,47 @@
 import csv
 import os
 
-path = os.path.dirname(os.path.abspath(__file__))
+path = os.path.dirname(os.path.abspath(__file__)) + "\\"
 
 
 def show_csv_content(file):
     csv_name = file + ".csv"
-    with open("\\" + path + file) as csv_file:
+    with open(path + csv_name) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         line_count = 0
         for row in csv_reader:
-                print(f'\t{row[0]} has a duration of {row[1]} , and it\'s a {row[2]} show. That I\'m currently {row[4]}')
+                print(f'\t{row[0]} has a duration of {row[1]} , '
+                      f'and it\'s a {row[2]} show. That I\'m currently {row[4]}')
                 line_count += 1
         print(f'Processed {line_count} lines.')
+    csv_file.close()
 
 
 def add_entry_csv(file, data):
     csv_name = file + ".csv"
-    with open("\\" + path + file, 'a') as csv_file:
+    with open(path + csv_name, 'a', newline='') as csv_file:
         csv_append = csv.writer(csv_file)
         csv_append.writerow(data)
+    csv_file.close()
 
 
-show_csv_content("Anime register")
+def retrieve_data_csv(file, name):
+    animeDataList = []
+    csv_name = file + '.csv'
+    with open(path + csv_name, 'r') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for row in csv_reader:
+            if name in row[0].lower():
+                animeDataList.append(row)
+    j = 0
+    index = 0
+    if len(animeDataList) != 1:
+        for j in range(0, len(animeDataList)):
+            print("[" + str(j) + "] " + (animeDataList[j])[0])
+        try:
+            index = input('\n[Animu-san] Ara ara, which of those you\'re refering to?\n[Kanna-chan] ')
+            print(animeDataList[int(index)])
+        except IndexError:
+            print('[Animu-san] Ara ara, that didn\'t exist! ')
+
+retrieve_data_csv('Anime register', 'idolm@ster')
