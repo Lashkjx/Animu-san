cf45cf754c5d6771efa25a69eb6b7779f862873a
Kanan Lashkjx <Lashkjx@gmail.com>
2019-06-29 14:37:42 -0300

    Added the option to retrieve information from Myanimelist.

---
+++ puya/pages/UI/MyAnimeList.py
@@ -0,0 +1,18 @@
+from googlesearch import search
+from bs4 import BeautifulSoup
+import requests
+
+
+def searchTitle(title):
+    for result in search(title + " mal", tld="com", num=1, stop=1, pause=0):
+        break
+    req = requests.get(result)
+    soup = BeautifulSoup(req.content, 'html.parser')
+    title = soup.select('h1')[0].text.strip()
+    type = soup.find(text='Type:', attrs={'class': 'dark_text'}.find_next_sibling().get_text())
+    duration_mal = soup.find(text='Duration:',
+                             attrs={'class': 'spaceit_pad', 'class': 'dark_text'}).next_sibling.strip()
+    duration = duration_mal[:duration_mal.find('.')] + "."
+    newEntry = [title, type, duration]
+    req.cookies.clear()
+    return newEntry
